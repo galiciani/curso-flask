3a81fed028416a587de72600f3fc59c7ecc24c81
Everton Luiz <everton.galiciani@gmail.com>
2023-02-21 15:05:01 -0300

    novo teste

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,10 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index.html')
 def index():
     return render_template('index.html', nome='faelinha')
+
+@app.route('/contato.html')
+def contato():
+    return render_template('contato.html')
